85668266cd8cd98b9242bb91b75aeb97fd19edac
Chidanand Sisodia <62592158+chidanand-sisodia@users.noreply.github.com>
2024-04-16 13:30:17 +0530

    updated certificates

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -5,31 +5,55 @@ const certifications = [
   {
     id: 1,
     title: "Certified React Developer",
-    imageUrl: "https://via.placeholder.com/720x400", // Placeholder image
+    imageUrl: "https://ik.imagekit.io/dpst6vive/certificates/Foundation_data_Google_page-0001.jpg?updatedAt=1713253730677", // Placeholder image
     certificateUrl: "https://example.com/certificate", // Placeholder link
   },
   {
     id: 2,
     title: "Full Stack Web Developer",
-    imageUrl: "https://via.placeholder.com/720x400",
+    imageUrl: "https://ik.imagekit.io/dpst6vive/certificates/Intro_ML_page-0001.jpg?updatedAt=1713253730723",
     certificateUrl: "https://example.com/certificate",
   },
   {
     id: 3,
     title: "Full Stack Web Developer",
-    imageUrl: "https://via.placeholder.com/720x400",
+    imageUrl: "https://ik.imagekit.io/dpst6vive/certificates/EDA_ML_page-0001.jpg?updatedAt=1713253730266",
     certificateUrl: "https://example.com/certificate",
   },
   {
     id: 4,
     title: "Full Stack Web Developer",
-    imageUrl: "https://via.placeholder.com/720x400",
+    imageUrl: "https://ik.imagekit.io/dpst6vive/certificates/machine_learning_pipelines_page-0001.jpg?updatedAt=1713253730209",
     certificateUrl: "https://example.com/certificate",
   },
   {
-    id: 4,
+    id: 5,
     title: "Full Stack Web Developer",
-    imageUrl: "https://via.placeholder.com/720x400",
+    imageUrl: "https://ik.imagekit.io/dpst6vive/certificates/chidanandsisodia-86019528%20AI-VILT_page-0001.jpg?updatedAt=1713253730659",
+    certificateUrl: "https://example.com/certificate",
+  },
+  {
+    id: 6,
+    title: "Full Stack Web Developer",
+    imageUrl: "https://ik.imagekit.io/dpst6vive/certificates/smile_detection_page-0001.jpg?updatedAt=1713253730585",
+    certificateUrl: "https://example.com/certificate",
+  },
+  {
+    id: 7,
+    title: "Full Stack Web Developer",
+    imageUrl: "https://ik.imagekit.io/dpst6vive/certificates/verzeo.jpg?updatedAt=1713253730495",
+    certificateUrl: "https://example.com/certificate",
+  },
+  {
+    id: 8,
+    title: "Full Stack Web Developer",
+    imageUrl: "https://ik.imagekit.io/dpst6vive/certificates/Internsship_studio_internship_page-0001.jpg?updatedAt=1713253730302",
+    certificateUrl: "https://example.com/certificate",
+  },
+  {
+    id: 8,
+    title: "Full Stack Web Developer",
+    imageUrl: "https://ik.imagekit.io/dpst6vive/certificates/Mass_technology_Internship_page-0001.jpg?updatedAt=1713253730552",
     certificateUrl: "https://example.com/certificate",
   },
   // Add more certifications as needed
@@ -52,14 +76,14 @@ function Certificates() {
               <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden hover:shadow-lg transition-shadow duration-300 ">
                 <a href={cert.certificateUrl} target="_blank" rel="noopener noreferrer">
                   <img 
-                    className="lg:h-48 md:h-36 w-full object-cover object-center transform hover:scale-110 transition-transform duration-300"
+                    className="lg:h-80 md:h-40 w-full object-cover object-center transform hover:scale-110 transition-transform duration-300"
                     src={cert.imageUrl}
                     alt={cert.title}
                   />
                 </a>
-                <div className="p-6 text-center">
+                {/* <div className="p-6 text-center">
                   <h2 className="text-lg text-gray-900 font-medium title-font mb-2">{cert.title}</h2>
-                </div>
+                </div> */}
               </div>
             </div>
           ))}
